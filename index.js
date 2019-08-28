c1538739eb60f06df5fe89db8c3fd143ec70d637
MrErdalUral <mrerdalural@gmail.com>
2019-08-28 12:02:53 +0300

    Added a placeholder default page to test if the host is working

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(express.json({ limit: '50mb' }));
 app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
 }));
-// app.get('/', (req, res) => {
-//     res.sendFile(`${__dirname}/index.html`);
-// });
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
 
 // app.get('/Certificates', (req, res) => {
 //     fs.readdir('./certificates', (err, files) => {
